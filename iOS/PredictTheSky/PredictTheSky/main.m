aeb5da767ca19cdc91ed5d7ad443a4ae8f7d596f
Nick Charlton <hello@nickcharlton.net>
2012-04-21 14:49:57 +0100

    Basic iOS Xcode Project.
    * iPhone & iPad
    * Storyboards.
    * ARC
    * Linked against iOS 5.

---
+++ iOS/PredictTheSky/PredictTheSky/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PredictTheSky
+//
+//  Created by Nick Charlton on 21/04/2012.
+//  Copyright (c) 2012 Nick Charlton. MIT Licensed.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
